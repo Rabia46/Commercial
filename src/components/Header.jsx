60e95070fa545aed7fcf445281a92e7a63d4ee30
Rabia Hussain <hrabia847@gmail.com>
2024-06-23 07:08:07 +0000

    first commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
 
 export default function AppHeader() {
@@ -24,17 +23,7 @@ export default function AppHeader() {
                         <Nav.Link href="#blog">Blog</Nav.Link>
                         <Nav.Link href="#contact">Contact</Nav.Link>
 
-                        {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                                 Another action
-                            </NavDropdown.Item>
-                        //     <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                        //     <NavDropdown.Divider />
-                        //     <NavDropdown.Item href="#action/3.4">
-                        //         Separated link
-                        //     </NavDropdown.Item>
-                        // </NavDropdown>  */}
+
                     </Nav>
                 </Navbar.Collapse>
             </Container>
